316f56b187096f53123797715f5d4ecf58ba6430
Waldo J. Saccaco <w.saccaco@gmail.com>
2016-11-18 12:59:15 -0500

    update data

---
+++ SQL.Script/Data.sql
@@ -0,0 +1,18 @@
+
+
+insert into marca values(1, 'Sony');
+insert into marca values(2, 'Samsung');
+insert into marca values(3, 'Apple');
+insert into marca values(4, 'Motorola');
+
+insert into modelo values(1, 'xperia 8', '20150101', 120.00, 12, 1)
+insert into modelo values(2, 'xperia 7', '20150101', 80.00, 10, 1)
+insert into modelo values(3, 'Z1', '20150101', 1200.00, 40, 1)
+
+insert into modelo values(4, 'S6', '20150101', 700.00, 20, 2)
+insert into modelo values(5, 'S5 mini', '20150101', 1220.00, 10, 2)
+insert into modelo values(6, 'Galaxy II', '20150101', 2020.00, 2, 2)
+
+insert into modelo values(7, 'iPhone5', '20150101', 900.00, 10, 3)
+insert into modelo values(8, 'iPhone5S', '20150101', 2220.00, 6, 3)
+insert into modelo values(9, 'iPhone6', '20150101', 1000.00, 22, 3)
