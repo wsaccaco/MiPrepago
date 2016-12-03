aef4950e0254a05c737d41823fddd53c7db52db6
Waldo J. Saccaco <w.saccaco@gmail.com>
2016-12-03 01:18:32 -0500

    modulo solicitud de modelo

---
+++ WEBS/WebSite/Models/Model_Proveedor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+    public class Model_Proveedor
+    {
+        public List<Proveeedor> ListaProveedor { get; set; }
+    }
+}
